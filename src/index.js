5296f26aae5e308bd9c09353caccaa881ff79220
Katelyn Luther <katelynluther87@aim.com>
2023-02-06 11:24:19 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const todoForm = document.getElementById('create-task-form');
+  todoForm.addEventListener('submit', handleSubmit)
 });
+
+function handleSubmit(event){
+  event.preventDefault();
+
+  const todoList = document.getElementById('tasks');
+  const todo = document.createElement('li');
+  todo.textContent = event.target['new-task-description'].value;
+  todoList.append(todo);
+}
